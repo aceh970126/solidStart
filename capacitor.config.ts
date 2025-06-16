214dbfb858e450cc9f20f06c664cf3bbc294c58b
aceh970126 <aceh970126@gmail.com>
2025-06-16 13:27:39 -0600

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'robert.solidcard.app',
+  appName: 'solidcard',
+  webDir: 'dist'
+};
+
+export default config;
